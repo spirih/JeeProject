f46d90f26b2b2397193fce4d47ab47447290fbc5
lou <lojanzie@gmail.com>
2024-04-29 15:27:43 +0200

    Initial commit

---
+++ src/main/java/com/example/testspringmaven/TestSpringMavenApplication.java
@@ -0,0 +1,26 @@
+package com.example.testspringmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestSpringMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringMavenApplication.class, args);
+    }
+
+    @PostMapping("/hello")
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+
+}
+
---
+++ src/test/java/com/example/testspringmaven/TestSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
