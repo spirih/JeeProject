ff8986a7b27011cbb2f2248b9acca7c890a4e33d
Kaputchino <andreasmulard@gmail.com>
2024-04-30 13:14:34 +0200

    hasher

---
+++ src/main/java/com/example/testspringmaven/utilitary/Hasher.java
@@ -0,0 +1,26 @@
+package com.example.testspringmaven.utilitary;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hasher {
+    Hasher(){}
+    public String hashing(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] b = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        BigInteger number = new BigInteger(1, b);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 64)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
+    }
+}
