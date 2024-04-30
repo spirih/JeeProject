3e8f1f5535ab3911aa4480775c19930d06203b26
Kaputchino <andreasmulard@gmail.com>
2024-04-30 09:55:07 +0200

    user

---
+++ src/main/java/com/example/testspringmaven/persistant/UsersEntity.java
@@ -0,0 +1,77 @@
+package com.example.testspringmaven.persistant;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "payaya")
+public class UsersEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "nickname")
+    private String nickname;
+    @Basic
+    @Column(name = "password")
+    private String password;
+    @Basic
+    @Column(name = "role")
+    private int role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (id != that.id) return false;
+        if (role != that.role) return false;
+        if (nickname != null ? !nickname.equals(that.nickname) : that.nickname != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (nickname != null ? nickname.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + role;
+        return result;
+    }
+}
